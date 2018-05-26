3f140a795e4e305ce09b6ca05228a6ef0c7719f4
Daniel Silva <dani3l.dxp@gmail.com>
2018-05-26 08:24:54 -0300

    Unidade 1 - Ambiente, Angular CLI e criando primeiro componente

--- Angular/meu-app/src/app/app.module.ts
+++ Angular/meu-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MeuComponenteComponent } from './meu-componente/meu-componente.component';
+import { NewComponentComponent } from './new-component/new-component.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuComponenteComponent,
+    NewComponentComponent
   ],
   imports: [
     BrowserModule
