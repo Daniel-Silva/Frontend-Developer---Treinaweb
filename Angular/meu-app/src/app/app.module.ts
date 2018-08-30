a6f18069e6e170a2bde86316d8dc1f2864983388
Daniel Silva <dani3l.dxp@gmail.com>
2018-08-30 19:17:08 -0300

    Unidade 5 - Binding

--- Angular/meu-app/src/app/app.module.ts
+++ Angular/meu-app/src/app/app.module.ts
@@ -4,14 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MeuComponenteComponent } from './meu-componente/meu-componente.component';
 import { NewComponentComponent } from './new-component/new-component.component';
-import { MeuComponent2Component } from './meu-component2/meu-component2.component'
+import { MeuComponent2Component } from './meu-component2/meu-component2.component';
+import { ClassComponentComponent } from './class-component/class-component.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     MeuComponenteComponent,
     NewComponentComponent,
-    MeuComponent2Component
+    MeuComponent2Component,
+    ClassComponentComponent
   ],
   imports: [
     BrowserModule
--- Angular/meu-app/src/app/meu-component2/meu-component2.component.ts
+++ Angular/meu-app/src/app/meu-component2/meu-component2.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MeuComponent2Component implements OnInit {
 
+
   nome: String = "Seja bem vindo Daniel Silva";
   isVisible = false;
 
